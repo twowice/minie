2efb6e7d47a73fe5205e5ffb2f1fe17cf20a42b2
kimsuhyeon <kshyunn12@gmail.com>
2025-11-07 22:11:38 +0900

    [fix] color mode theme hydration 오류 해결 #2
    provider에서 제공하는 컬러모드가 서버와 클라이언트가 달라 리렌더링되는 문제를 provider를 csr으로 보내어 mount 되었을 때 color mode provider가 작동하도록 해결했습니다

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 // app/layout.tsx
-
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header";
--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,15 +1,19 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+import { useState, useEffect } from "react";
 
 export function Provider(props: ColorModeProviderProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      {mounted && <ColorModeProvider {...props} />}
     </ChakraProvider>
-  )
+  );
 }
