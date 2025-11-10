f3bb97b66c5f1d1c11a5a76f54a7ae8d0d17127f
kimsuhyeon <kshyunn12@gmail.com>
2025-11-10 12:03:39 +0900

    [Feat] 장바구니 Drawer 설정 #3

--- components/ShoppingCartDrawer.tsx
+++ components/ShoppingCartDrawer.tsx
@@ -5,42 +5,92 @@ import {
   Button,
   CloseButton,
   IconButton,
-  Icon,
-  createIcon,
+  Tabs,
+  Box,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CartIcon } from "./ui/CartIcon";
 
-const CartIcon = createIcon({
-  displayName: "CartIcon",
-  viewBox: "0 0 24 24",
-  path: (
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-      fill="none"
-      stroke="currentColor"
-    />
-  ),
-});
-
-export default function ShoppingCartDrawer() {
+export default function ShoppingCartDrawer({
+  headerHeight,
+}: {
+  headerHeight: number;
+}) {
   const [isCartActivity, setIsCartActivity] = useState(false);
 
   return (
-    <IconButton
-      aria-label="장바구니"
-      onClick={() => {
-        setIsCartActivity((prev) => !prev);
-      }}
-      cursor="pointer"
+    <Drawer.Root
+      open={isCartActivity}
+      onOpenChange={(details) => setIsCartActivity(details.open)}
     >
-      <CartIcon
-        w={{ base: 5, md: 6 }}
-        h={{ base: 5, md: 6 }}
-        color={isCartActivity ? "#FA6D6D" : "black"}
-      />
-    </IconButton>
+      <Drawer.Trigger asChild>
+        <IconButton
+          aria-label="장바구니"
+          onClick={() => {
+            setIsCartActivity((prev) => !prev);
+          }}
+          cursor="pointer"
+        >
+          <CartIcon
+            w={{ base: 5, md: 6 }}
+            h={{ base: 5, md: 6 }}
+            color={isCartActivity ? "#FA6D6D" : "black"}
+          />
+        </IconButton>
+      </Drawer.Trigger>
+
+      <Portal>
+        <Drawer.Positioner top={`${headerHeight}px`} right="0" bottom="0">
+          <Drawer.Content background={"white"} maxW={"530px"} w="100%">
+            <Tabs.Root defaultValue="cart" color={"black"}>
+              <Drawer.Header
+                justifyContent="space-between"
+                alignItems="center"
+                color={"black"}
+                px="40px"
+              >
+                <Tabs.List>
+                  <Tabs.Trigger value="cart">
+                    <Drawer.Title
+                      fontSize="lg"
+                      fontWeight="medium"
+                      color={"black"}
+                    >
+                      장바구니
+                    </Drawer.Title>
+                  </Tabs.Trigger>
+                  <Tabs.Trigger value="like">
+                    <Drawer.Title
+                      fontSize="lg"
+                      fontWeight="medium"
+                      color={"black"}
+                    >
+                      좋아요
+                    </Drawer.Title>
+                  </Tabs.Trigger>
+                </Tabs.List>
+                <Box>전체 삭제</Box>
+              </Drawer.Header>
+
+              <Drawer.Body px="40px">
+                <Tabs.Content value="cart">
+                  <Box p={4}>장바구니에 담긴 상품이 없습니다.</Box>
+                </Tabs.Content>
+
+                <Tabs.Content value="like">
+                  <Box p={4}>좋아요에 담긴 상품이 없습니다.</Box>
+                </Tabs.Content>
+              </Drawer.Body>
+
+              <Drawer.Footer px="40px">
+                <Button variant="outline">Cancel</Button>
+
+                <Button>Save</Button>
+              </Drawer.Footer>
+            </Tabs.Root>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
   );
 }
