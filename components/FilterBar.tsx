2089f64c954ee3f0fa03f32872a523803e0cd6ab
yebyn <yebyn19981114@gmail.com>
2025-11-12 00:31:42 +0900

    [feat] 필터 팝업내성별, 가격 필터 기능 구현

--- components/FilterBar.tsx
+++ components/FilterBar.tsx
@@ -21,9 +21,11 @@ export default function FilterBar() {
       { id: 1, name: '상품 1', gender: 'male', price: 9000 },
       { id: 2, name: '상품 2', gender: 'female', price: 12000 },
       { id: 3, name: '상품 3', gender: 'male', price: 18000 },
-      { id: 4, name: '상품 4', gender: 'male', type: 'MyType', price: 25000 },
+      { id: 4, name: '상품 4', gender: 'male', type: 'MyType', price: 20000 },
       { id: 5, name: '상품 5', gender: 'male', price: 5000 },
    ];
+
+   const maxPrice = items.reduce((prev, curr) => (curr.price > prev.price ? curr : prev)).price;
 
    const { open, onOpen, onClose } = useDisclosure();
    const [male, setMale] = useState(false);
@@ -32,52 +34,82 @@ export default function FilterBar() {
    const [myType, setMyType] = useState(false);
    const [data, setData] = useState(items);
 
+   const [filterMale, setFilterMale] = useState(false);
+   const [filterFemale, setFilterFemale] = useState(false);
+   const [filterBoth, setFilterBoth] = useState(true);
+   const [filterPrice, setFilterPrice] = useState([]);
+
    useEffect(() => {
-    let selected = items;
+      let selected = items;
 
-    if (male && !female) {
-        selected = selected.filter((item) => item.gender === 'male')
-    } else if (!male && female) {
-        selected = selected.filter((item) => item.gender === 'female')
-    } else if (male && female ) {
-        selected = selected.filter((item) => item.gender === 'male' || item.gender === 'female')
-    }
+      if (male && !female) {
+         selected = selected.filter(item => item.gender === 'male');
+      } else if (!male && female) {
+         selected = selected.filter(item => item.gender === 'female');
+      } else if (male && female) {
+         selected = selected.filter(item => item.gender === 'male' || item.gender === 'female');
+      }
 
-    if ( myType ) {
-        selected = selected.filter((item) => item.type === 'MyType')
-    }
+      if (myType) {
+         selected = selected.filter(item => item.type === 'MyType');
+      }
 
-    setData(selected)
+      setData(selected);
+   }, [male, female, myType]);
 
-   }, [male, female, myType])
+   const filteredData = () => {
+      let checked = items;
 
-   const [filterData, setFilterData] = useState({
-      genders: [],
-      price: null,
-   });
-   const [popupGenders, setPopupGenders] = useState([]);
-   const [popupPrice, setPopupPrice] = useState(null);
+      if (filterBoth || filterFemale || filterMale) {
+         if (filterBoth) {
+         } else if (filterMale && filterFemale) {
+            checked = checked.filter(item => item.gender === 'male' || item.gender === 'female');
+         } else if (filterMale) {
+            checked = checked.filter(item => item.gender === 'male');
+         } else if (filterFemale) {
+            checked = checked.filter(item => item.gender === 'female');
+         }
+      }
+
+      if (filterPrice.length > 0) {
+         checked = checked.filter(item => filterPrice.some(price => item.price <= price));
+      }
+      return checked.length;
+   };
 
    const handleFilterDialog = () => {
-      setPopupGenders(filterData.genders);
-      setPopupPrice(filterData.price);
+      setFilterMale(true);
+      setFilterFemale(true);
+      setFilterBoth(true);
+      setFilterPrice([10000, 15000, 20000]);
       onOpen();
    };
 
    const handleSaveDialog = () => {
-      setFilterData({
-         genders: popupGenders,
-         price: popupPrice,
-      });
+      if (filterBoth) {
+         setMale(false);
+         setFemale(false);
+      } else if (filterMale && filterFemale) {
+         setMale(true);
+         setFemale(true);
+      } else {
+         setMale(filterMale);
+         setFemale(filterFemale);
+      }
       onClose();
+      setDialog(false);
    };
+
+   const togglePrice = (price) => {
+    setFilterPrice(prev => prev.includes(price) ? prev.filter(p => p !== price) : [...prev, price])
+   }
 
    return (
       <Flex mb={2} direction={{ base: 'column', md: 'row' }} flexDirection={'column'}>
          <Box display={'flex'} justifyContent={'space-between'} backgroundColor={'rgba(204,204,204,0.16)'} p={'8px'}>
             <Box display={'flex'} gap={'10px'}>
                <Button
-               color={dialog ? '#fa6d6d' : 'rgba(0,0,0,0.32)'}
+                  color={dialog ? '#fa6d6d' : 'rgba(0,0,0,0.32)'}
                   bg={'white'}
                   border={'1px solid rgba(0,0,0,0.32)'}
                   borderRadius={'2px'}
@@ -87,16 +119,16 @@ export default function FilterBar() {
                   justifyContent={'center'}
                   alignItems={'center'}
                   onClick={() => {
-                     onOpen(); //Dialog 트리거
+                     handleFilterDialog(); //Dialog 트리거
                      setDialog(prev => !prev);
                   }}
                >
-                <MdFilterListAlt />
+                  <MdFilterListAlt />
                </Button>
                <Button
                   color={male ? 'white' : 'rgba(0,0,0,0.32)'}
                   bg={male ? '#fa6d6d' : 'white'}
-                  border={male ? 'transparant' : '1px solid rgba(0,0,0,0.32)'}
+                  border={male ? 'transparent' : '1px solid rgba(0,0,0,0.32)'}
                   borderRadius={'2px'}
                   w={'24px'}
                   h={'24px'}
@@ -111,7 +143,7 @@ export default function FilterBar() {
                <Button
                   color={female ? 'white' : 'rgba(0,0,0,0.32)'}
                   bg={female ? '#fa6d6d' : 'white'}
-                  border={female ? 'transparant' : '1px solid rgba(0,0,0,0.32)'}
+                  border={female ? 'transparent' : '1px solid rgba(0,0,0,0.32)'}
                   borderRadius={'2px'}
                   w={'24px'}
                   h={'24px'}
@@ -126,7 +158,7 @@ export default function FilterBar() {
                <Button
                   color={myType ? 'white' : 'rgba(0,0,0,0.32)'}
                   bg={myType ? '#fa6d6d' : 'white'}
-                  border={myType ? 'transparant' : '1px solid rgba(0,0,0,0.32)'}
+                  border={myType ? 'transparent' : '1px solid rgba(0,0,0,0.32)'}
                   borderRadius={'2px'}
                   h={'24px'}
                   fontSize={'12px'}
@@ -167,17 +199,48 @@ export default function FilterBar() {
                               성별
                            </Text>
                            <HStack>
-                              <Checkbox.Root variant={'solid'} colorPalette={'red'}>
+                              <Checkbox.Root
+                                 variant={'solid'}
+                                 colorPalette={'red'}
+                                 checked={filterBoth}
+                                 onCheckedChange={e => {
+                                    setFilterBoth(e.checked);
+                                    if (e.checked) {
+                                       setFilterMale(false);
+                                       setFilterFemale(false);
+                                    }
+                                 }}
+                              >
                                  <Checkbox.HiddenInput />
                                  <Checkbox.Control />
                                  <Checkbox.Label>남녀무관</Checkbox.Label>
                               </Checkbox.Root>
-                              <Checkbox.Root variant={'solid'} colorPalette={'red'}>
+                              <Checkbox.Root
+                                 variant={'solid'}
+                                 colorPalette={'red'}
+                                 checked={filterMale}
+                                 onCheckedChange={e => {
+                                    setFilterMale(e.checked);
+                                    if (e.checked) {
+                                       setFilterBoth(false);
+                                    }
+                                 }}
+                              >
                                  <Checkbox.HiddenInput />
                                  <Checkbox.Control />
                                  <Checkbox.Label>남성</Checkbox.Label>
                               </Checkbox.Root>
-                              <Checkbox.Root variant={'solid'} colorPalette={'red'}>
+                              <Checkbox.Root
+                                 variant={'solid'}
+                                 colorPalette={'red'}
+                                 checked={filterFemale}
+                                 onCheckedChange={e => {
+                                    setFilterFemale(e.checked);
+                                    if (e.checked) {
+                                       setFilterBoth(false);
+                                    }
+                                 }}
+                              >
                                  <Checkbox.HiddenInput />
                                  <Checkbox.Control />
                                  <Checkbox.Label>여성</Checkbox.Label>
@@ -189,17 +252,32 @@ export default function FilterBar() {
                               가격
                            </Text>
                            <HStack>
-                              <Checkbox.Root variant={'solid'} colorPalette={'red'}>
+                              <Checkbox.Root
+                                 variant={'solid'}
+                                 colorPalette={'red'}
+                                 checked={filterPrice.includes(10000)}
+                                 onCheckedChange={() => togglePrice(10000)}
+                              >
                                  <Checkbox.HiddenInput />
                                  <Checkbox.Control />
                                  <Checkbox.Label>10,000원 이하</Checkbox.Label>
                               </Checkbox.Root>
-                              <Checkbox.Root variant={'solid'} colorPalette={'red'}>
+                              <Checkbox.Root
+                                 variant={'solid'}
+                                 colorPalette={'red'}
+                                 checked={filterPrice.includes(15000)}
+                                 onCheckedChange={() => togglePrice(15000)}
+                              >
                                  <Checkbox.HiddenInput />
                                  <Checkbox.Control />
                                  <Checkbox.Label>15,000원 이하</Checkbox.Label>
                               </Checkbox.Root>
-                              <Checkbox.Root variant={'solid'} colorPalette={'red'}>
+                              <Checkbox.Root
+                                 variant={'solid'}
+                                 colorPalette={'red'}
+                                 checked={filterPrice.includes(20000)}
+                                 onCheckedChange={() => togglePrice(20000)}
+                              >
                                  <Checkbox.HiddenInput />
                                  <Checkbox.Control />
                                  <Checkbox.Label>20,000원 이하</Checkbox.Label>
@@ -217,21 +295,21 @@ export default function FilterBar() {
                   <Dialog.Footer>
                      <Button
                         onClick={() => {
-                           onClose();
+                           handleSaveDialog();
                            setDialog(prev => !prev);
                         }}
                         bg="#FA6D6D"
                         color="white"
                      >
-                        {length}개의 상품 보러가기
+                        {filteredData()}개의 상품 보러가기
                      </Button>
                   </Dialog.Footer>
                   <Dialog.CloseTrigger asChild>
                      <CloseButton
                         size={'sm'}
                         onClick={() => {
-                           onClose();
-                           setDialog(prev => !prev);
+                           handleSaveDialog();
+                           setDialog(false);
                         }}
                         bg="white"
                         color="black"
