a65abb3b3b3625ea5a8b21f3e87cda914e3c67ae
Young-Jun-Park18 <dudwns4619@naver.com>
2025-11-11 14:42:36 +0900

    [design] 로그인 버튼, 구분선, 소셜 로그인 버튼 레이아웃 완성 #5

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,16 +6,28 @@
 
 import { Container, VStack, Box, Heading, Link} from "@chakra-ui/react"; // 헤더에 사용된 라이브러리
 import { Input, Text, Flex } from "@chakra-ui/react"; // 입력폼에 사용된 라이브러리
+import { Button } from "@chakra-ui/react"; // 일반 및 소셜 로그인 버튼에 사용된 라이브러리
+import { FcGoogle } from "react-icons/fc";  // 구글 아이콘
+import { RiKakaoTalkFill } from "react-icons/ri";  // 카카오 아이콘
+import { SiNaver } from "react-icons/si";  // 네이버 아이콘
 
 export default function LoginPage(){
   return(
     // body 전체 영역
-    <Box px="40px" py="24px" bg="gray.100" flex="1"> 
+    <Box 
+      px="40px" 
+      py="24px" 
+      flex="1"
+      display="flex"
+      alignItems="center" // 세로 중앙 정렬
+      justifyContent="center" // 가로 중앙 정렬
+      
+    > 
 
       {/* 로그인 컨테이너 */}
       <Container maxW = "md">
         {/* 세로형 책장 */}
-        <VStack gap ={8}>
+        <VStack gap ={4}>
 
           {/* 첫 번째 책꽃이(제목) */}
           <Box textAlign="center">
@@ -62,7 +74,7 @@ export default function LoginPage(){
             </VStack>
           </Box>
 
-         {/* 세 번째 책꽃이(회원가입) */}
+         {/* 세 번째 책꽃이(회원가입 버튼) */}
           <Box w="full">
             <Flex justify="flex-end">
               <Link 
@@ -81,6 +93,82 @@ export default function LoginPage(){
             </Flex>
           </Box>
 
+          {/* 네 번째 책꽃이(로그인 버튼) */}
+          <Box w="full">
+            <Button 
+              w="full"
+              bg="#FA6D6D"
+              height="48px"
+              borderRadius="4px"
+              color="#ffffff"
+              fontSize="16px"
+              fontWeight="medium"
+              >
+              로그인
+            </Button>
+          </Box>
+
+          {/* 다섯 번째 책꽃이(구분선) */}
+          <Flex align="center" w="full"> {/* VStack이 세로형 책꽃이였다면, Flex는 가로 방향으로 정렬하는 책꽃이, 세로는 중앙에 배치*/}
+            <Box flex="1" h="1px" bg="rgba(0, 0, 0, 0.4)" /> {/* flex="1"을 해주면 남는 공간을 모두 차지한다는 뜻 */}
+            <Text px={4} fontSize="12px" color="rgba(0, 0, 0, 0.4)"> {/* px={4} => px="16px"과 같은 뜻, x축 좌우 여백을 16px만큼 줌.*/}
+              또는
+            </Text>
+            <Box flex="1" h="1px" bg="rgba(0, 0, 0, 0.4)" />
+          </Flex>
+
+          {/* 여섯 번째 책꽃이(구글 로그인) */}
+          <Box w="full">
+            <Button w="full"
+              bg="#ffffff"
+              border="1px solid"
+              borderRadius="4px"
+              borderColor="rgba(0, 0, 0, 0.3)"
+              fontSize="16px"
+              color="rgba(0, 0, 0, 0.8)"
+              height="48px"
+              fontWeight="bold"
+            >
+              <Box position="absolute" left="16px">  {/* 아이콘 왼쪽 고정 */}
+                <FcGoogle style={{ width: '24px', height: '24px' }} />
+              </Box>
+                Google 로그인
+            </Button>
+          </Box>
+
+          {/* 일곱 번째 책꽃이(카카오 로그인) */}
+          <Box w="full">
+            <Button w="full"
+              bg="#FEE500"
+              borderRadius="4px"
+              fontSize="16px"
+              color="rgba(0, 0, 0, 0.8)"
+              height="48px"
+              fontWeight="bold"
+            >
+              <Box position="absolute" left="16px">
+                <RiKakaoTalkFill style={{width: "24px", height: "24px"}}/>
+              </Box>
+              Kakao 로그인
+            </Button>
+          </Box>
+
+          {/* 여덟 번째 책꽃이(네이버 로그인) */}
+          <Box w="full">
+            <Button w="full"
+              bg="#03C75A"
+              borderRadius="4px"
+              fontSize="16px"
+              color="#ffffff"
+              height="48px"
+              fontWeight="bold"
+            >
+              <Box position="absolute" left="20px">
+                <SiNaver style={{width: "16px", height: "16px"}}/>
+              </Box>
+              Naver 로그인
+            </Button>
+          </Box>
 
         </VStack>
       </Container>
