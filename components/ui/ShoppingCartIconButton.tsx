d44dbb48dd65f4746b65639658e48afe3ff8e985
kimsuhyeon <kshyunn12@gmail.com>
2025-11-14 12:43:09 +0900

    [design] 장바구니 아이콘 배경색 추가 #4

--- components/ui/ShoppingCartIconButton.tsx
+++ components/ui/ShoppingCartIconButton.tsx
@@ -16,6 +16,7 @@ export default function ShoppingCartIconButton({
       aria-label="장바구니"
       cursor="pointer"
       position={"relative"}
+      bg={"white"}
       onClick={() => {
         setIsCartActivity((prev) => !prev);
       }}
